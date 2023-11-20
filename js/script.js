09c8b95ddd004017132bffc6ed43830b0a8ad0b6
Qmans2000 <144332680+Qmans2000@users.noreply.github.com>
2023-11-20 20:35:58 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('field1').addEventListener('input', function() {
-    const checkbox = document.querySelector('.checkbox');
-    checkbox.classList.toggle('visible', this.value.trim() == '6655 8844 2233 5599');
-});
